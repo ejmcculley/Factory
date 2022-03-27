e600cf9f722593cabc5ce0a8e02ac12a594b8fab
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-03-26 20:49:58 -0700

    Add public virtual Engineer and Engineer Id to Machine class

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -8,9 +8,10 @@ namespace Factory.Models
     {
       this.JoinEntities = new HashSet<EngineerMachine>();
     }
-
     public int MachineId { get; set; }
     public string Name { get; set; }
+    public int EngineerId { get; set; }
+    public virtual Engineer Engineer { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
   }
 }
